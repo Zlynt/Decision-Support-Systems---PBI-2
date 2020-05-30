bc5e800e463405e1039d8fbde8ddd3718a5f828c
ZenJB <0zenjb0@gmail.com>
2020-05-30 15:13:06 +0100

    Criado projeto

---
+++ SAD_PBI2/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("============== PBI 2 ================");
+		System.out.println("Ivan Teixeira");
+		System.out.println("Josué Ferreira");
+		System.out.println("=====================================");
+
+	}
+
+}

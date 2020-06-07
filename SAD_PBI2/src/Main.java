4ea134f9bbe1b071ca495dcabcef01819e07fa11
patriotHunter <yoshua93@outlook.com>
2020-06-07 16:43:01 +0100

    Adding some essential source

--- SAD_PBI2/src/Main.java
+++ SAD_PBI2/src/Main.java
@@ -7,12 +7,17 @@ import java.io.File;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		System.out.println("============== PBI 2 ================");
 		System.out.println("Ivan Teixeira");
 		System.out.println("Josué Ferreira");
 		System.out.println("=====================================");
 
+		
+		CSVLoader loader = new CSVLoader();
+		//loader.setSource(file);
+		Instances data = loader.getDataSet();
+		
 	}
 
 }

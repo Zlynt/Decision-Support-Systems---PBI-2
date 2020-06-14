c5e9c9972275191875e4f3f69fa68f6f6f9c0b24
patriotHunter <yoshua93@outlook.com>
2020-06-14 14:57:39 +0100

    Adding new files KTR and testing basics Eclipse

--- SAD_PBI2/src/Main.java
+++ SAD_PBI2/src/Main.java
@@ -3,8 +3,14 @@ import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
+import weka.associations.Apriori;
+import weka.core.converters.ConverterUtils.DataSource;
 
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+
 
 public class Main {
 
@@ -13,18 +19,29 @@ public class Main {
 		System.out.println("Ivan Teixeira");
 		System.out.println("Josué Ferreira");
 		System.out.println("=====================================");
-
 		
+		File file = new File("C:\\sad\\implementacao\\TASKDATA1.csv");
+		File outputFile = new File("C:\\sad\\implementacao\\TASKDATA1.arff");
+
 		CSVLoader loader = new CSVLoader();
-		//loader.setSource(file);
+		loader.setSource(file);
 		Instances data = loader.getDataSet();
 		
 		
 		ArffSaver saver = new ArffSaver();
 		saver.setInstances(data);
-		//saver.setFile(outputFile);
+		saver.setFile(outputFile);
 		saver.writeBatch();
 		
+//		String dataset = "C:\\sad\\implementacao\\TASKDATA1.arff";
+//		DataSource source = new DataSource(dataset);
+//		
+//		//load data
+//		Instances data = source.getDataSet();
+//		
+//		Apriori model = new Apriori();
+//		model.buildAssociations(data);
+//		System.out.println(model);
 	}
 
 }

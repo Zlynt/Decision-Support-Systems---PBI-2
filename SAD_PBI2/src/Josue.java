2c79db181f78964e447f691ab4039a3b2b17643d
patriotHunter <yoshua93@outlook.com>
2020-06-22 00:35:20 +0100

    Adding some basic for Josue java class

--- SAD_PBI2/src/Josue.java
+++ SAD_PBI2/src/Josue.java
@@ -5,6 +5,22 @@ public class Josue {
 	}
 	
 	public void executar_codigo() {
-		//Todo o código corre aqui
+		TASKDATA1 taskdata1 = new TASKDATA1();
+		TASKDATA2 taskdata2 = new TASKDATA2();
+		
+		try {
+//			System.out.println("[TASKDATA4] Processing the CSV input...");
+//			taskdata4.save_arff(taskdata4.csv_to_instances());
+//			
+//			System.out.println("[TASKDATA4] Loading arff...");
+//			Instances taskdata4_instances = taskdata4.load_arff();
+//
+//			System.out.println("[TASKDATA4] Mining association rules...");
+//			System.out.println(taskdata4.apriori_mine_association_rules(taskdata4_instances));
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }

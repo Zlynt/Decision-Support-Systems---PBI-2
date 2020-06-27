22745c42221f2f475c7d42e0276542d37bea8dad
Zlynt <ivan.goncalo@gmail.com>
2020-06-27 16:29:15 +0100

    Code is now more beautiful
    Code of task data 4 is more beautiful and understadable

--- SAD_PBI2/src/Ivan.java
+++ SAD_PBI2/src/Ivan.java
@@ -11,15 +11,7 @@ public class Ivan {
 		TASKDATA3 taskdata3 = new TASKDATA3();
 		
 		try {
-			//System.out.println("[TASKDATA4] Processing the CSV input...");
-			//taskdata4.save_arff(taskdata4.csv_to_instances());
-			
-			//System.out.println("[TASKDATA4] Loading arff...");
-			//Instances taskdata4_instances = taskdata4.load_arff();
-
-			//System.out.println("[TASKDATA4] Mining association rules...");
-			//System.out.println(taskdata4.apriori_mine_association_rules(taskdata4_instances));
-
+			//Task Data 3
 			System.out.println("[TASKDATA3] Processing the CSV input...");
 			taskdata3.save_arff(taskdata3.csv_to_instances());
 			
@@ -28,6 +20,16 @@ public class Ivan {
 
 			System.out.println("[TASKDATA3] Mining association rules...");
 			System.out.println(taskdata3.apriori_mine_association_rules(taskdata3_instances));
+			
+			//Task Data 4
+			System.out.println("[TASKDATA4] Processing the CSV input...");
+			taskdata4.save_arff(taskdata4.csv_to_instances());
+			
+			System.out.println("[TASKDATA4] Loading arff...");
+			Instances taskdata4_instances = taskdata4.load_arff();
+
+			System.out.println("[TASKDATA4] Mining association rules...");
+			System.out.println(taskdata4.apriori_mine_association_rules(taskdata4_instances));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
---
+++ SAD_PBI2/src/TASKDATA4/TransactionList.java
@@ -0,0 +1,121 @@
+package TASKDATA4;
+
+import java.text.Collator;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class TransactionList {
+	// Transaction list
+	private LinkedList<Transaction> transactionList;
+	// Products present on the transactions
+	private LinkedList<String> productList;
+
+	// Constructor
+	public TransactionList() {
+		transactionList = new LinkedList<Transaction>();
+		productList = new LinkedList<String>();
+	}
+
+	// Sort any LinkedList<String>
+	private void sortStringLinkedList(LinkedList<String> list) {
+		Collections.sort(list, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return Collator.getInstance().compare(o1, o2);
+			}
+		});
+	}
+
+	// Add a product to the product list
+	public void addProduct(String product) {
+		if (!productList.contains(product)) {
+			productList.add(product);
+			sortStringLinkedList(productList);
+		}
+	}
+
+	// Check if a transaction already exists
+	public boolean transactionExists(int transactionID) {
+		for (int i = 0; i < transactionList.size(); i++)
+			if (transactionList.get(i).getID() == transactionID)
+				return true;
+
+		return false;
+	}
+
+	// Get a transaction position in the LinkedList
+	public int getTransactionIndex(int transactionID) {
+		for (int i = 0; i < transactionList.size(); i++)
+			if (transactionList.get(i).getID() == transactionID)
+				return i;
+
+		return -1;
+	}
+
+	// Add transaction to the list
+	public void addTransaction(Transaction transaction) {
+		if (transactionExists(transaction.getID())) {
+			int transactionIndex = getTransactionIndex(transaction.getID());
+
+			// Add the products to the existing transaction
+			for (int i = 0; i < transaction.getProducts().size(); i++) {
+				transactionList.get(transactionIndex).addProduct(transaction.getProducts().get(i));
+				addProduct(transaction.getProducts().get(i));
+			}
+		} else {
+			transactionList.add(transaction);
+			for (int i = 0; i < transaction.getProducts().size(); i++) {
+				addProduct(transaction.getProducts().get(i));
+			}
+		}
+	}
+
+	// Remove transaction from the list
+	public void removeTransaction(Transaction transaction) {
+		transactionList.remove(transaction);
+	}
+
+	// Return the data has arff String
+	public String toARFF() {
+		String arff = "";
+
+		arff += "@relation TASKDATA3\n\n"; // Add TaskData relation name (in this case it is the TaskData 3
+
+		// Add the transaction id attribute
+		arff += "@attribute TID {";
+		// Add the transactions to the possible values list
+		for (int i = 0; i < transactionList.size(); i++) {
+			arff += transactionList.get(i).getID() + ",";
+		}
+		// Remove last comma and close the transaction attribute
+		arff = arff.substring(0, arff.length() - 1);
+		arff += "}\n";
+
+		// Add the products to the attribute list
+		for (int i = 0; i < productList.size(); i++) {
+			arff += "@attribute " + productList.get(i) + " {y,n}\n";
+		}
+
+		// Add the data (transactions)
+		arff += "\n@data\n";
+		for (int i = 0; i < transactionList.size(); i++) {
+			Transaction currentTransaction = transactionList.get(i);
+
+			arff += currentTransaction.getID() + ",";
+
+			// Set the products
+			for (int a = 0; a < productList.size(); a++) {
+				if (currentTransaction.getProducts().contains(productList.get(a)))
+					arff += "y,";
+				else
+					arff += "n,";
+			}
+			// Remove extra comma
+			arff = arff.substring(0, arff.length() - 1);
+			arff += "\n";
+		}
+
+		return arff;
+	}
+}

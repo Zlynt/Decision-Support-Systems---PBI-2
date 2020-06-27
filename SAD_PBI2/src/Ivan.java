59a8f07fbddbd50a2a7b202db9e4d5c30f34cc9d
Zlynt <ivan.goncalo@gmail.com>
2020-06-27 16:30:15 +0100

    Formated code

--- SAD_PBI2/src/Ivan.java
+++ SAD_PBI2/src/Ivan.java
@@ -3,28 +3,28 @@ import weka.core.Instances;
 
 public class Ivan {
 	public Ivan() {
-		
+
 	}
-	
+
 	public void executar_codigo() {
 		TASKDATA4 taskdata4 = new TASKDATA4();
 		TASKDATA3 taskdata3 = new TASKDATA3();
-		
+
 		try {
-			//Task Data 3
+			// Task Data 3
 			System.out.println("[TASKDATA3] Processing the CSV input...");
 			taskdata3.save_arff(taskdata3.csv_to_instances());
-			
+
 			System.out.println("[TASKDATA3] Loading arff...");
 			Instances taskdata3_instances = taskdata3.load_arff();
 
 			System.out.println("[TASKDATA3] Mining association rules...");
 			System.out.println(taskdata3.apriori_mine_association_rules(taskdata3_instances));
-			
-			//Task Data 4
+
+			// Task Data 4
 			System.out.println("[TASKDATA4] Processing the CSV input...");
 			taskdata4.save_arff(taskdata4.csv_to_instances());
-			
+
 			System.out.println("[TASKDATA4] Loading arff...");
 			Instances taskdata4_instances = taskdata4.load_arff();
 
@@ -34,6 +34,6 @@ public class Ivan {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }

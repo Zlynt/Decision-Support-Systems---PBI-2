50e0925cbb64e1c97b0d59efb2a726547c7fb6d1
patriotHunter <yoshua93@outlook.com>
2020-06-28 16:25:36 +0100

    Adjusting something

--- SAD_PBI2/src/TASKDATA1.java
+++ SAD_PBI2/src/TASKDATA1.java
@@ -7,6 +7,7 @@ import java.text.Collator;
 import java.util.Comparator;
 import java.util.LinkedList;
 
+import TASKDATA3.TransactionList;
 import weka.associations.Apriori;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
@@ -35,9 +36,13 @@ public class TASKDATA1 extends TASKDATA{
 		tmp_data_loaded = tmp_data_loaded.replace("@relationTASKDATA2", "");
 		String[] data_loaded = tmp_data_loaded.split("\n");
 		
+		boolean read_data = false;
+		TransactionList transactionList = new TransactionList();
+		
 		Reader inputString = new StringReader(transactionList.toARFF());
 		BufferedReader reader = new BufferedReader(inputString);
 		ArffReader arff = new ArffReader(reader);
+		return arff.getData();
 	}
 
 }

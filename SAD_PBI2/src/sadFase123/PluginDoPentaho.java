440d2ff7df829bf824a295765f83ae4e0ae94114
Zlynt <ivan.goncalo@gmail.com>
2020-07-14 19:19:09 +0100

    TASKDATA3 Added

--- SAD_PBI2/src/sadFase123/PluginDoPentaho.java
+++ SAD_PBI2/src/sadFase123/PluginDoPentaho.java
@@ -1,28 +1,23 @@
 package sadFase123;
 
-import java.io.PrintWriter;
-
-import sadFase12.TASKDATA3;
 import weka.associations.Apriori;
 import weka.core.Instances;
 
 public class PluginDoPentaho {
+	private TASKDATA3 taskData3;
 	private TASKDATA4 taskData4;
 
 	public PluginDoPentaho() {
 		taskData4 = new TASKDATA4();
-	}
-
-	public String getStuff() {
-		return "hum,";
+		taskData3 = new TASKDATA3();
 	}
 
 	public String getTASKDATA1Rules() {
-		return "I'm a Dummy Rule from TASKDATA1";
+		return "I'm a Rule from TASKDATA1";
 	}
 
 	public String getTASKDATA2Rules() {
-		return "I'm a Dummy Rule from TASKDATA2";
+		return "I'm a Rule from TASKDATA2";
 	}
 
 	public String getTASKDATA3Rules() throws Exception {
@@ -30,13 +25,20 @@ public class PluginDoPentaho {
 
 		String debug = "";
 
-		TASKDATA3 taskdata3 = new TASKDATA3();
 		try {
+
+
+			// Generate the CSV file and save it
+			System.out.println("[TASKDATA3] Generating CSV...");
+			taskData3.generateCSV();
+			System.out.println("[TASKDATA3] CSV generated.");
+			debug += "CSV_GENERATED ";
 			
+			/*
 			// Task Data 3
 
 			taskdata3.generateCSV();
-			
+
 			System.out.println("[TASKDATA3] Processing the CSV input...");
 			taskdata3.save_arff(taskdata3.csv_to_instances());
 
@@ -44,10 +46,11 @@ public class PluginDoPentaho {
 			Instances taskdata3_instances = taskdata3.load_arff();
 
 			System.out.print("[TASKDATA3] Mining association rules...");
-			String taskData3MinedAssociationRules = taskdata3.apriori_mine_association_rules(taskdata3_instances, 0.01, 0.7);
+			String taskData3MinedAssociationRules = taskdata3.apriori_mine_association_rules(taskdata3_instances, 0.01,
+					0.7);
 			System.out.println("done!");
 			System.out.println(taskData3MinedAssociationRules);
-			return taskData3MinedAssociationRules;
+			return taskData3MinedAssociationRules;*/
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -56,40 +59,34 @@ public class PluginDoPentaho {
 	}
 
 	public String getTASKDATA4Rules() {
-		String debug = "";
 		try {
 
 			// Generate the CSV file and save it
 			System.out.println("[TASKDATA4] Generating CSV...");
 			taskData4.generateCSV();
 			System.out.println("[TASKDATA4] CSV generated.");
-			debug += "CSV_GENERATED ";
 
 			// Transform CSV into ARFF and save it
 			System.out.println("[TASKDATA4] Converting the CSV to ARFF...");
 			taskData4.csvToArff();
 			System.out.println("[TASKDATA4] ARFF saved.");
-			debug += "CSV_CONVERTED ARFF_SAVED ";
-			
+
 			System.out.println("[TASKDATA4] Mining associations...");
 			Instances instances = taskData4.load_arff();
-			debug += "GOT_INSTANCES ";
-			
-			Apriori apriori = new Apriori(); //Isto é a causa dos problemas. O Pentaho Server não se dá com o Apriori
+
+			Apriori apriori = new Apriori();
 			apriori.setLowerBoundMinSupport(0.01);
 			apriori.buildAssociations(instances);
-			//String associationRules = model.toString();
-			System.out.println("[TASKDATA4] Association rules mined!1");
-			debug += "RULES_MINED ";
-			
+			System.out.println("[TASKDATA4] Association rules mined!");
+
+			return apriori.toString();
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println(e.toString());
-			debug += "MESSED_UP";
 		}
 
-		return debug;
+		return "";
 	}
 }
---
+++ SAD_PBI2/src/sadFase123/TASKDATA3.java
@@ -0,0 +1,7 @@
+package sadFase123;
+
+public class TASKDATA3 extends TASKDATA {
+	public TASKDATA3() {
+		super("3");
+	}
+}

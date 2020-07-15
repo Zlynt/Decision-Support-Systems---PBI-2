402275d5f077fd27f54f70d769e16d68635cf0e8
Zlynt <ivan.goncalo@gmail.com>
2020-07-15 14:32:01 +0100

    MainTest

--- SAD_PBI2/src/sadFase123/MainTest.java
+++ SAD_PBI2/src/sadFase123/MainTest.java
@@ -6,12 +6,25 @@ public class MainTest {
 
 	public static void main(String[] args) throws KettleException {
 		// TODO Auto-generated method stub
-		PluginDoPentaho plugin = new PluginDoPentaho();
+		PluginDoPentaho pluginDoPentaho = new PluginDoPentaho();
 		
-		plugin.getTASKDATA1Rules();
-		plugin.getTASKDATA2Rules();
-		plugin.getTASKDATA3Rules();
-		plugin.getTASKDATA4Rules();
+		//pluginDoPentaho.getTASKDATA1Rules();
+		//pluginDoPentaho.getTASKDATA2Rules();
+		//pluginDoPentaho.getTASKDATA3Rules();
+		String currentTask = pluginDoPentaho.getTASKDATA4Rules().split("Best rules found:\n\n")[1];
+		for(int i = 0; i < currentTask.split("\n").length; i++) {
+			String currentRule = currentTask.split("\n")[i];
+			currentRule = currentRule.split(" lift:")[0];
+			
+			String currentConfidence = currentRule.split("    ")[currentRule.split("    ").length-1];
+			currentRule = currentRule.substring(0, currentRule.length() - (currentConfidence+"    ").length());
+			currentConfidence = currentConfidence.replace("<", "");
+			currentConfidence = currentConfidence.replace(">", "");
+			currentConfidence = currentConfidence.replace("conf:(", "");
+			currentConfidence = currentConfidence.substring(0, currentConfidence.length()-1);
+			
+			System.out.println("<p>"+currentRule+" | "+currentConfidence+"</p>");
+		}
 	}
 
 }

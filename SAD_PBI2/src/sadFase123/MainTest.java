db4c7d4cfc76cea70a40455d614f1812694ee444
Zlynt <ivan.goncalo@gmail.com>
2020-07-16 22:43:33 +0100

    Update

--- SAD_PBI2/src/sadFase123/MainTest.java
+++ SAD_PBI2/src/sadFase123/MainTest.java
@@ -22,8 +22,34 @@ public class MainTest {
 			currentConfidence = currentConfidence.replace(">", "");
 			currentConfidence = currentConfidence.replace("conf:(", "");
 			currentConfidence = currentConfidence.substring(0, currentConfidence.length()-1);
+			//currentRule = currentRule.replace("=Bought", "");
+			String currentRuleLeft = currentRule.split("==>")[0];
+			String currentRuleRight = currentRule.split("==>")[1];
 			
-			System.out.println("<p>"+currentRule+" | "+currentConfidence+"</p>");
+			String ruleToPrint = "";
+			if(currentRuleLeft.split("=Bought").length == 2) {
+				ruleToPrint += currentRuleLeft.split("=Bought")[0] + " ( Support("+currentRuleLeft.split("=Bought")[1].replace(" ", "")+") )";
+			}else {
+				for(int p = 0; p < currentRuleLeft.split("=Bought").length-1; p++) {
+					ruleToPrint += currentRuleLeft.split("=Bought")[p] + " && ";
+				}
+				ruleToPrint = ruleToPrint.substring(0, ruleToPrint.length()-4);
+				ruleToPrint += " ) Support("+currentRuleLeft.split("=Bought")[currentRuleLeft.split("=Bought").length-1]+") )";
+			}
+			
+			ruleToPrint += " ==> ";
+			if(currentRuleRight.split("=Bought").length == 2) {
+				ruleToPrint += currentRuleRight.split("=Bought")[0] + " ( Support("+currentRuleRight.split("=Bought")[1].replace(" ", "")+") )";
+			}else {
+				for(int p = 0; p < currentRuleRight.split("=Bought").length-1; p++) {
+					ruleToPrint += currentRuleRight.split("=Bought")[p] + " && ";
+				}
+				ruleToPrint = ruleToPrint.substring(0, ruleToPrint.length()-4);
+				ruleToPrint += " ) Support("+currentRuleRight.split("=Bought")[currentRuleRight.split("=Bought").length-1]+") )";
+			}
+			
+			
+			System.out.println(ruleToPrint);
 		}
 	}
 
